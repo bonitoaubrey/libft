592408e286dc4ae5859b8660704bdb9339988ed3
dav <dav@debian>
2021-05-20 23:39:01 +0300

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -2,20 +2,20 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-	size_t	i;
-	size_t	len;
+	char	*new_str;
+	int		start;
+	int		finish;
 
 	if (!s1 || !set)
 		return (NULL);
-	i = 0;
-	while (s1[i] != '0' && ft_strchr(set, s1[i]))
-		i++;
-	len = ft_strlen(s1 + i);
-	while (ft_strchr(set, s1[i + len]))
-		len--;
-	str = ft_substr(s1, i, len + 1);
-	if (!str)
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	finish = ft_strlen(s1 + start);
+	while (ft_strchr(set, s1[start + finish]))
+		finish--;
+	new_str = ft_substr(s1, start, finish + 1);
+	if (!new_str)
 		return (NULL);
-	return (str);
+	return (new_str);
 }
