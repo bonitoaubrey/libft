bf9328fd95a9955bbd64d3840978208cbc1699bd
dav <dav@debian>
2021-05-20 18:22:22 +0300

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,15 +2,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && (i < n))
+	while (n-- && (s1[i] || s2[i]))
 	{
-		if ((unsigned char)s1[i] == (unsigned char)s2[i])
-			i++;
-		else
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] -(unsigned char) s2[i]);
+		i++;
 	}
 	return (0);
 }
