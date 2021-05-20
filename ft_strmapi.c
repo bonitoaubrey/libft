713a25456a33a656aad4fba43064fe84c2f1d809
dav <dav@debian>
2021-05-20 11:26:21 +0300

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -2,20 +2,18 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char			*str;
+	char			*new_str;
 	unsigned int	i;
 
+	new_str = malloc(ft_strlen(s) + 1);
+	if (!new_str)
+		return (NULL);
 	i = 0;
-	if (!s)
-		return (NULL);
-	str = malloc(ft_strlen(s) + 1);
-	if (!str)
-		return (NULL);
 	while (s[i])
 	{
-		str[i] = f(i, s[i]);
+		new_str[i] = f(i, s[i]);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
+	new_str[i] = '\0';
+	return (new_str);
 }
