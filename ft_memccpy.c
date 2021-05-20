30c8c39fcf923317d8a9c961e1df2d0d108f9179
dav <dav@debian>
2021-05-20 11:40:34 +0300

    memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,16 +1,16 @@
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	while (n--)
 	{
 		if (*((unsigned char *)src) == (char)c)
 		{
-			*((unsigned char *)dst) = *((unsigned char *)src);
-			return (++dst);
+			*((unsigned char *)dest) = *((unsigned char *)src);
+			return (++dest);
 		}
 		else
-			*((unsigned char *)dst++) = *((unsigned char *)src++);
+			*((unsigned char *)dest++) = *((unsigned char *)src++);
 	}
 	return (NULL);
 }
