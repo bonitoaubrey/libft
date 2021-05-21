9dd9a37bf22bfff97a33d8f095ea7e49dc51a972
dav <dav@debian>
2021-05-21 08:45:11 +0300

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,20 +2,15 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*c1;
-	const unsigned char	*c2;
-
-	c1 = s1;
-	c2 = s2;
 	while (n--)
 	{
-		if (*c1 == *c2)
-		{
-			c1++;
-			c2++;
-		}
+		if (*((unsigned char *)s1) != *((unsigned char *)s2))
+			return (*(unsigned char *)(s1) - *((unsigned char *)s2));
 		else
-			return (*c1 - *c2);
+		{
+			s1++;
+			s2++;
+		}
 	}
 	return (0);
 }
