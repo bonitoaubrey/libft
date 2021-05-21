bf3a58b4307011182141c64ade75ad2bc9d21c23
dav <dav@debian>
2021-05-21 11:12:39 +0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -5,16 +5,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*new_str;
 	int		i;
 
+	i = 0;
 	new_str = malloc(len + 1);
 	if (!new_str)
 		return (NULL);
-	i = 0;
-	while (len--)
-	{
-		new_str[i] = s[start];
-		i++;
-		start++;
-	}
+	while (len-- && s[start])
+		new_str[i++] = s[start++];
 	new_str[i] = '\0';
 	return (new_str);
 }
