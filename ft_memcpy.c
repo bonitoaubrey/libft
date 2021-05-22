d29c31ebe777f44ef616729a728c1cd827274fe0
DAV <corpseDAV@gmail.com>
2021-05-22 06:05:59 -0400

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,16 +1,8 @@
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	i;
-
-	i = 0;
-	if (dst == NULL && src == NULL)
-		return (NULL);
 	while (n--)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dst);
+		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	return (dest);
 }
