195b6606371067382c45a0ad937096b1875319a8
DAV <corpseDAV@gmail.com>
2021-05-22 11:24:25 -0400

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,27 +2,17 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	char	*tmp;
-	size_t	i;
+	char	*new_str;
+	int		i;
 
+	new_str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new_str)
+		return (NULL);
 	i = 0;
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1 && s2)
-		i = ft_strlen(s2) + 1;
-	else if (s1 && s2)
-		i = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(sizeof(*str) * i);
-	if (!str)
-		return (NULL);
-	tmp = str;
-	if (s1)
-		while (*s1)
-			*tmp++ = *s1++;
-	if (s2)
-		while (*s2)
-			*tmp++ = *s2++;
-	*tmp = '\0';
-	return (str);
+	while (*s1)
+		new_str[i++] = *s1++;
+	while (*s2)
+		new_str[i++] = *s2++;
+	new_str[i] = '\0';
+	return (new_str);
 }
