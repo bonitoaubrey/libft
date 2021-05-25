f0119b5159170d02d409db77b0c8b4f0b10c773b
DAV <corpseDAV@gmail.com>
2021-05-25 05:19:29 -0400

    memmove and memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,7 +2,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
+	int	i;
+
+	i = 0;
 	while (n--)
-		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
 	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -2,12 +2,14 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	if (!dst && !src)
-		return (NULL);
-	if (dst > src)
-		while (len--)
-			((char *)dst)[len] = ((char *)src)[len];
-	else
+	if (dst < src)
 		ft_memcpy(dst, src, len);
+	else
+	{
+		dst += len;
+		src += len;
+		while (len--)
+			*(unsigned char *)--dst = *(unsigned char *)--src;
+	}
 	return (dst);
 }
