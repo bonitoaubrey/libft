cb93439253b90e81add90fb618bd57a717255104
DAV <corpseDAV@gmail.com>
2021-05-27 12:48:00 -0400

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,19 +1,9 @@
 #include "libft.h"
 
-static void	ft_print_num(long int n, int fd)
-{
-	if (n > 9)
-		ft_print_num(n / 10, fd);
-	ft_putchar_fd((n % 10 + '0'), fd);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_print_num((long int)n * (-1), fd);
-	}
-	else
-		ft_print_num((long int)n, fd);
+	char	*str;
+
+	str = ft_itoa(n);
+	write(fd, str, ft_strlen(str));
 }
