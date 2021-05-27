a2be37959ff931d9560ef8e73548f2b1f1f66e36
Bonito Aubrey <bonitoaubrey@gmail.com>
2021-05-27 18:55:04 +0300

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,6 +4,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n--)
 		if (*s1++ != *s2++)
-			return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
+			return (*--s1 - *--s2);
 	return (0);
 }
