bbb054f71260935d618159f74e3606752463de14
DAV <corpseDAV@gmail.com>
2021-05-28 02:42:51 -0400

    strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,12 +5,10 @@ char	*ft_strrchr(const char *s, int c)
 	int	len;
 
 	len = ft_strlen(s);
-	if (!c)
+	if (!(char)c)
 		return ((char *)s + len);
 	while (len--)
-	{
 		if (s[len] == (char)c)
 			return ((char *)s + len);
-	}
 	return (NULL);
 }
