f47be0ed158806552d845203e2b2bf0279b79510
dav <dav@debian>
2021-06-03 12:29:35 +0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,23 +1,23 @@
 #include "libft.h"
 
-int	ft_is_space(char c)
+int	ft_isspace(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n'
-		|| c == '\r' || c == '\v' || c == '\f');
+	return (c == '\t' || c == '\n' || c == ' '
+		|| c == '\v' || c == '\r' || c == '\f');
 }
 
 int	ft_atoi(const char *s)
 {
 	int	num;
-	int	i;
 	int	minus;
+	int	i;
 
 	num = 0;
-	i = 0;
 	minus = 1;
-	while (ft_is_space(s[i]))
+	i = 0;
+	while (ft_isspace(s[i]))
 		i++;
-	if (s[i] == '-' || s[i] == '+')
+	if (s[i] == '+' || s[i] == '-')
 		if (s[i++] == '-')
 			minus *= -1;
 	while (ft_isdigit(s[i]))
