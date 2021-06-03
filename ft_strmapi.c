4d5575acd776912b24ad09d1030dccef6b21c9ad
dav <dav@debian>
2021-06-03 12:08:28 +0300

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
 	char			*new_str;
 	unsigned int	i;
@@ -8,12 +8,9 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	new_str = malloc(ft_strlen(s) + 1);
 	if (!new_str)
 		return (NULL);
-	i = 0;
-	while (s[i])
-	{
+	i = -1;
+	while (s[++i])
 		new_str[i] = f(i, s[i]);
-		i++;
-	}
 	new_str[i] = '\0';
 	return (new_str);
 }
