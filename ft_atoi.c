002af4501a258616b369fae745e502193b36512c
dav <dav@debian>
2021-06-03 15:29:27 +0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,25 +2,23 @@
 
 int	ft_isspace(char c)
 {
-	return (c == '\t' || c == '\n' || c == ' '
-		|| c == '\v' || c == '\r' || c == '\f');
+	return (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\r' || c == '\v' || c == '\f');
 }
 
 int	ft_atoi(const char *s)
 {
-	int	num;
+	int	n;
 	int	minus;
-	int	i;
 
-	num = 0;
+	n = 0;
 	minus = 1;
-	i = 0;
-	while (ft_isspace(s[i]))
-		i++;
-	if (s[i] == '+' || s[i] == '-')
-		if (s[i++] == '-')
+	while (ft_isspace(*s))
+		s++;
+	if (*s == '-' || *s == '+')
+		if (*s++ == '-')
 			minus *= -1;
-	while (ft_isdigit(s[i]))
-		num = num * 10 + s[i++] - '0';
-	return (num * minus);
+	while (ft_isdigit(*s))
+		n = n * 10 + *s++ - '0';
+	return (n * minus);
 }
