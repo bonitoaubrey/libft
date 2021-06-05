1c7fc927621c662a67e9b51c62fa3007a289c03a
DAV <corpseDAV@gmail.com>
2021-06-05 00:59:28 -0400

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,6 +6,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		while (n--)
 			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
 	else
-		return (ft_memcpy(dest, src, n));
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
