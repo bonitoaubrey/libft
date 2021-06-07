0f1ae5c37ee773d9ec00192285aab725c9a99a58
dav <dav@debian>
2021-06-07 07:50:46 +0300

    memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,9 +1,9 @@
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *b, int c, size_t size)
 {
-	while (n--)
-		if (*(unsigned char *)s++ == (unsigned char)c)
-			return ((void *)--s);
+	while (size--)
+		if (*(unsigned char *)b++ == (unsigned char)c)
+			return ((void *)--b);
 	return (NULL);
 }
