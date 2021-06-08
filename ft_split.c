b2a308ea21f995bb294bca403a7e673ac243747d
dav <dav@debian>
2021-06-08 08:47:18 +0300

    split

--- ft_split.c
+++ ft_split.c
@@ -1,65 +1,65 @@
 #include "libft.h"
 
-int	ft_number_of_words(char *s, char c)
+int	ft_word_count(char *s, char c)
 {
-	int	number_of_words;
-	int	is_word;
+	size_t	word_count;
+	int		is_word;
 
-	number_of_words = 0;
 	is_word = 0;
+	word_count = 0;
 	while (*s)
 	{
-		if (*s == c && is_word == 1)
-			is_word = 0;
-		else if (*s != c && !is_word)
+		if (is_word == 0 && *s != c)
 		{
 			is_word = 1;
-			number_of_words++;
+			word_count++;
 		}
+		else if (is_word == 1 && *s == c)
+			is_word = 0;
 		s++;
 	}
-	return (number_of_words);
+	return (word_count);
 }
 
-int	ft_word_len(char *s, char c)
+size_t	ft_lenword(char *s, char c)
 {
-	int	len;
+	size_t	lenword;
 
-	len = 0;
+	lenword = 0;
 	while (*s != c && *s++)
-		len++;
-	return (len);
+		lenword++;
+	return (lenword);
 }
 
-void	*ft_free(char **new_arr, int i)
+void	*ft_free(char **arr, int i)
 {
 	while (i--)
-		free (new_arr[i]);
-	free (new_arr);
+		free(arr[i]);
+	free (arr);
 	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	char	**new_arr;
-	int		number_of_words;
+	char	**arr;
+	int		word_count;
 	int		i;
 
-	number_of_words = ft_number_of_words((char *)s, c);
-	new_arr = malloc(sizeof(char *) * (number_of_words + 1));
-	if (!new_arr)
+	word_count = ft_word_count((char *)s, c);
+	arr = malloc(sizeof(char *) * (word_count + 1));
+	if (!arr)
 		return (NULL);
 	i = 0;
-	while (number_of_words--)
+	while (word_count--)
 	{
 		while (*s == c && *s)
 			s++;
-		new_arr[i] = ft_substr(s, 0, ft_word_len((char *)s, c));
-		if (!new_arr[i])
-			return (ft_free(new_arr, i));
-		s += ft_word_len((char *)s, c);
+		arr[i] = ft_substr(s, 0, ft_lenword((char *)s, c));
+		if (!arr[i])
+			return (ft_free(arr, i));
+		s += ft_lenword((char *)s, c);
 		i++;
 	}
-	new_arr[i] = NULL;
-	return (new_arr);
+	arr[i] = NULL;
+	return (arr);
 }
