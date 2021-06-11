6efd07e345b6fce07604b341d352be332205f873
bonitoaubrey <bonitoaubrey@gamil.com>
2021-06-11 10:47:49 +0300

    memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,16 +1,16 @@
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t size)
 {
-	while (n--)
+	while (size--)
 	{
-		if (*(unsigned char *)src == (unsigned char)c)
-		{
-			*(unsigned char *)dest = *(unsigned char *)src;
-			return (++dest);
-		}
+		if (*(unsigned char *)src != (unsigned char)c)
+			*(unsigned char *)dst++ = *(unsigned char *)src++;
 		else
-			*(unsigned char *)dest++ = *(unsigned char *)src++;
+		{
+			*(unsigned char *)dst = *(unsigned char *)src;
+			return (++dst);
+		}
 	}
 	return (NULL);
 }
